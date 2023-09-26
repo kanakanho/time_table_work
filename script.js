0178f193258dfe0e08c89e9a5953bb3e000e9c3e
kanakanho <kahoshibakichi@gmail.com>
2023-09-26 16:39:58 +0900

    jsonのurlを変更

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function populate() {
-    const requestURL = 'https://raw.githubusercontent.com/kanakanho/time_table_work/sub2/data_table_last.json';
+    const requestURL = "https://raw.githubusercontent.com/kanakanho/time_table_work/main/1_2.json";
     const request = new Request(requestURL);
 
     const response = await fetch(request);
@@ -9,32 +9,38 @@ async function populate() {
 }
 
 function time_table(obj) {
-    const table = document.querySelector('.table');
+    const table = document.querySelector(".table");
     const tables = obj[0].day_main;
     let count = 0;
     for (const box of tables) {
         count++;
         console.log(box);
-        const divDay = document.createElement('div');
-        divDay.className = 'day';
+        const divDay = document.createElement("div");
+        divDay.className = "day";
 
-        const myPara1 = document.createElement('p');
-        const myPara2 = document.createElement('p');
-        const myPara3 = document.createElement('p');
-        const myPara4 = document.createElement('p');
-        const myPara5 = document.createElement('a');
-        const br = document.createElement('br');
-        const myPara6 = document.createElement('a');
+        const myPara1 = document.createElement("p");
+        const myPara2 = document.createElement("p");
+        const myPara3 = document.createElement("p");
+        const myPara4 = document.createElement("p");
+        const myPara5 = document.createElement("a");
+        const br = document.createElement("br");
+        const myPara6 = document.createElement("a");
 
-
-        if (box.main[0].name === "月" || box.main[0].name === "火" || box.main[0].name === "水" || box.main[0].name === "木" || box.main[0].name === "金" || count <= 5) {
+        if (
+            box.main[0].name === "月" ||
+            box.main[0].name === "火" ||
+            box.main[0].name === "水" ||
+            box.main[0].name === "木" ||
+            box.main[0].name === "金" ||
+            count <= 5
+        ) {
         } else {
-            myPara1.className = 'name';
-            myPara2.className = 'teacher';
-            myPara3.className = 'room';
-            myPara4.className = 'book';
-            myPara5.className = 'teams';
-            myPara6.className = 'moodle';
+            myPara1.className = "name";
+            myPara2.className = "teacher";
+            myPara3.className = "room";
+            myPara4.className = "book";
+            myPara5.className = "teams";
+            myPara6.className = "moodle";
         }
 
         myPara1.textContent = `${box.main[0].name}`;
@@ -57,7 +63,6 @@ function time_table(obj) {
             myPara6.href = box.main[0].moodle;
             myPara6.target = "_blank";
         }
-
 
         divDay.appendChild(myPara1);
         divDay.appendChild(myPara2);
